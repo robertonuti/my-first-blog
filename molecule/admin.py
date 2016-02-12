f735898bfc051e8c0623074af620a269ac1456c8
roberto <roberto.nuti@gmail.com>
2016-02-12 18:16:25 +0100

    RN Django Molecule PROVA app, first commit

---
+++ molecule/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Molecule
+
+admin.site.register(Molecule)
+
+# Register your models here.
---
+++ molecule/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Molecule',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=150)),
+                ('struct', models.TextField()),
+                ('target', models.CharField(max_length=200)),
+                ('activity', models.FloatField(default=None, null=True, blank=True)),
+            ],
+        ),
+    ]
